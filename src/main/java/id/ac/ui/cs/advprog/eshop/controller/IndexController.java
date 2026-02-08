01fc53e17e144812b113861229c821b72d6fcc93
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-02-09 00:02:28 +0700

    add functional tests and required routes

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/IndexController.java
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+}
